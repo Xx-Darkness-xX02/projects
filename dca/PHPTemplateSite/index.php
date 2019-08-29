1760175eb54a484af948600847a35919d7d60520
k.roelofs <kjell.roelofs@student.rocvf.nl>
2019-08-29 16:25:16 +0200

    hallo

---
+++ dca/PHPTemplateSite/index.php
@@ -0,0 +1,24 @@
+<?php
+require "header.php";
+?>
+
+    <link rel="stylesheet" href="includes/index.css">
+
+
+
+
+        <div id="wrapper">
+
+                <div class="artikel1"></div>
+                <div class="artikel2"></div>
+                <div class="artikel3"></div>
+                <div class="artikel4"></div>
+                <div id="navigatiebalk"></div>
+        </div>
+
+    </main>
+
+
+<?php
+
+require "footer.php";
