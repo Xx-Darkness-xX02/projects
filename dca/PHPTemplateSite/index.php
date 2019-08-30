c3f6ac3fd521dbdaeb19ac0ee61c9f35e4914805
k.roelofs <kjell.roelofs@student.rocvf.nl>
2019-08-30 13:37:22 +0200

    home pagina af

--- dca/PHPTemplateSite/index.php
+++ dca/PHPTemplateSite/index.php
@@ -3,17 +3,66 @@ require "header.php";
 ?>
 
     <link rel="stylesheet" href="includes/index.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 
 
-
-
+    <main>
         <div id="wrapper">
 
-                <div class="artikel1"></div>
-                <div class="artikel2"></div>
-                <div class="artikel3"></div>
-                <div class="artikel4"></div>
-                <div id="navigatiebalk"></div>
+            <div class="artikel1">
+                <h1>Rekenmachine</h1>
+                <small><p>-posted at 30/08/19</p></small>
+                <p class="paragraaf1">Dit is het eerste php project(je) wat ik heb gemaakt tijdens mijn stage.
+                    De rekenmachine werkt maar hij is nog niet user-vriendelijk.</p>
+                <div class="extra1">
+                    <i class="fas fa-folder"></i>
+                    <a href="externalPages/rekenmachine.php">verder lezen</a>
+                </div>
+            </div>
+
+            <div class="artikel2">
+                <h1>Login systeem</h1>
+                <small><p>-posted at 30/08/19</p></small>
+                <p class="paragraaf2">dit is het 3e project dat ik heb gemaakt. ik heb deze met behulp van een tutorial
+                    gemaakt.
+                    tijdens het maken van het systeem heb ik veel geleerd.</p>
+                <div class="extra2">
+                    <i class="fas fa-folder"></i>
+                    <a href="externalPages/login.php">verder lezen</a>
+                </div>
+            </div>
+
+            <div class="artikel3">
+                <h1>Nieuwsbrief</h1>
+                <small><p>-posted at 30/08/19</p></small>
+                <p class="paragraaf3">Dit is het 2e project(je) dat ik heb gemaakt. Ik heb dit gemaakt met de tutorial van Joey.</p>
+                <div class="extra3">
+                    <i class="fas fa-folder"></i>
+                    <a href="externalPages/nieuwsbrief.php">verder lezen</a>
+                </div>
+            </div>
+
+            <div class="artikel4">
+                <h1>Forum</h1>
+                <small><p>-posted at 30/08/19</p></small>
+                <p class="paragraaf4">dit project is nog niet van start gegaan. het kan zijn dat er een andere komt of dat ik hier aan ga beginnen.</p>
+                <div class="extra4">
+                    <i class="fas fa-folder"></i>
+                    <a href="external/img/l.php">verder lezen</a>
+                </div>
+            </div>
+
+            <div id="navigatiebalk">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Products</a></li>
+                    <li><a href="#">About us</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">Rekenmachine</a></li>
+                    <li><a href="#">Login systeem</a></li>
+                    <li><a href="#">Nieuwsbrief</a></li>
+                </ul>
+            </div>
         </div>
 
     </main>
